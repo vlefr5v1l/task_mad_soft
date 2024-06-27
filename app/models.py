8c4326c56e22237ced17b09eaf8f687a2f9f9c32
vlefr <vlefr5v1l@gmail.com>
2024-06-27 23:38:30 +0300

    fix bugs

--- app/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from .database import Base
-
-class Meme(Base):
-    __tablename__ = "memes"
-
-    id = Column(Integer, primary_key=True, index=True)
-    text = Column(String, index=True)
-    image_url = Column(String)
